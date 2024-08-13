b8625f833ed0dad6b4960b4c5e6450221a9238d2
sathya-reddy1658 <ktr6304@gmail.com>
2024-08-14 00:39:22 +0530

    added more models with brief about each

--- frontend/models/models.js
+++ frontend/models/models.js
@@ -1,56 +1,104 @@
 let models = [
     {
         "link": "https://sketchfab.com/3d-models/electromagnetic-induction-62da62419b1f409188e7a3958b6473e2/embed",
-        "name": "EMI"
+        "name": "EMI",
+        "brief": "Electromagnetic Induction"
     },
     {
         "link": "https://sketchfab.com/3d-models/isometric-chemistry-lab-996c0447b4654d6e8b8144993cbc6473/embed",
-        "name": "chemLab"
+        "name": "chemLab",
+        "brief": "Isometric Chemistry Lab"
     },
     {
         "link": "https://sketchfab.com/3d-models/edta-calcium-complexe-f5eaad269ac6475abc99e1952408cc2d/embed",
-        "name": "EDTA"
+        "name": "EDTA",
+        "brief": "EDTA Calcium Complex"
     },
     {
         "link": "https://sketchfab.com/3d-models/stylized-simply-visualization-of-dna-51f5ab2ffc364f60be00289eb6909535/embed",
-        "name": "DNA"
+        "name": "DNA",
+        "brief": "Stylized DNA Visualization"
     },
     {
         "link": "https://sketchfab.com/3d-models/structure-of-neuron-be679731525c457cb01036fcf25afc37/embed",
-        "name": "neuron"
+        "name": "neuron",
+        "brief": "Structure of Neuron"
     },
     {
         "link": "https://sketchfab.com/3d-models/eukaryotic-cell-cross-section-74f714127a8c4211bb1a2cac7195fb1a/embed",
-        "name": "eeukCell"
+        "name": "eeukCell",
+        "brief": "Eukaryotic Cell Cross-Section"
     },
     {
         "link": "https://sketchfab.com/3d-models/animal-cell-0b15c013059844d7a26c1f16752f8b61/embed",
-        "name": "animalCell"
+        "name": "animalCell",
+        "brief": "Animal Cell"
     },
     {
         "link": "https://sketchfab.com/3d-models/atom-b505bc59e6fb48569e4f032a0734f743/embed",
-        "name": "atom"
+        "name": "atom",
+        "brief": "Atom"
     },
     {
         "link": "https://sketchfab.com/3d-models/ironman-arc-reactor-with-sound-d87547f21f904cfa954f4cf77a1409ac/embed",
-        "name": "arcReactor"
+        "name": "arcReactor",
+        "brief": "Ironman Arc Reactor"
     },
     {
         "link": "https://sketchfab.com/3d-models/realistic-engine-turboprop-f5ea812ccd484b34b66d127a28630863/embed",
-        "name": "turbine"
+        "name": "turbine",
+        "brief": "Realistic Engine Turboprop"
     },
     {
         "link": "https://sketchfab.com/3d-models/engine-eea9d9252ab14298b50699a471dc2cee/embed",
-        "name": "engine"
+        "name": "engine",
+        "brief": "Engine"
     },
     {
         "link": "https://sketchfab.com/3d-models/3d-graph-multiple-linear-regression-model-be676c8f7e3942fd96ef04bcd64321ca/embed",
-        "name": "linReg"
+        "name": "linReg",
+        "brief": "3D Graph: Multiple Linear Regression Model"
     },
     {
         "link": "https://sketchfab.com/3d-models/3d-graph-of-thermodynamic-behavior-77733f8513a348a39099d0f6a346067d/embed",
-        "name": "thermo"
+        "name": "thermo",
+        "brief": "3D Graph of Thermodynamic Behavior"
+    },
+    {
+        "link": "https://sketchfab.com/3d-models/animation-human-heart-775d6629622740de8a5ed61a959c7506/embed",
+        "name": "heart",
+        "brief": "Animation of Human Heart"
+    },
+    {
+        "link": "https://sketchfab.com/3d-models/skeleton-6db4f2130c544f628cb349f532d49b1b/embed",
+        "name": "skeleton",
+        "brief": "Skeleton"
+    },
+    {
+        "link": "https://sketchfab.com/3d-models/transformers-dc8b41f6657b42209b63d88d293b26dc/embed",
+        "name": "transformers",
+        "brief": "Transformers"
+    },
+    {
+        "link": "https://sketchfab.com/3d-models/arduino-uno-wifi-card-model-esp8266mod-f953d84010d346fc8b1719d79d4c4a58/embed",
+        "name": "esp82",
+        "brief": "Arduino UNO WiFi Card Model ESP8266"
+    },
+    {
+        "link": "https://sketchfab.com/3d-models/rfid-rc522-arduino-uno-compatible-16501915e98143e49930a755a87272b3/embed",
+        "name": "RFID",
+        "brief": "RFID RC522 Arduino UNO Compatible"
+    },
+    {
+        "link": "https://sketchfab.com/3d-models/arduino-uno-85e4a60ec0294e2c83fbafa6051a3681/embed",
+        "name": "arduinoUno",
+        "brief": "Arduino UNO"
+    },
+    {
+        "link": "https://sketchfab.com/3d-models/male-digestive-system-adb79910fd784d02bebe666fe0e1c640/embed",
+        "name": "digestionSYstem",
+        "brief": "Male Digestive System"
     }
-]
+];
 
 module.exports = models;
